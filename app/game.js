faedbc270a58b80e0e849797d42cabb3440c70bb
marine-spaak <marine.spaak@gmail.com>
2023-09-08 12:10:33 +0200

    header OK on gamePage

--- app/game.js
+++ app/game.js
@@ -1,15 +1,45 @@
-import { View } from 'react-native';
-import { useRouter } from 'expo-router';
+import { View, Text, SafeAreaView, ScrollView } from 'react-native';
+import { Stack, useRouter } from 'expo-router';
 
+import { COLORS, SIZES, icons, images } from '../constants';
+
+import { ScreenHeaderBtn } from '../components';
 import GameComponent from "../components/game/Game";
 
 const Game = () => {
   const router = useRouter();
 
   return (
-    <View>
-      <GameComponent />
-    </View>
+    // Couleur du background de l'app
+    <SafeAreaView style={{ flex:1, backgroundColor: COLORS.lightWhite }}>
+
+      <Stack.Screen
+        options={{
+          // Couleur du header
+          headerStyle: { backgroundColor: COLORS.grayWhite },
+          headerShadowVisible: true,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconURL={icons.menu} dimension="60%"/>
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconURL={images.burger} dimension="70%"/>
+          ),
+          headerTitle: "",
+        }}
+      />
+
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View
+          style={{
+            flex: 1,
+            padding: SIZES.medium
+          }}
+        >
+          <GameComponent />
+        </View>
+      </ScrollView>
+
+    </SafeAreaView>
   )
 }
 
--- components/game/game.style.js
+++ components/game/game.style.js
@@ -1,8 +1,27 @@
 import { StyleSheet } from "react-native";
 
-import { COLORS, SIZES } from "../../constants/theme.js";
+import { COLORS, SIZES } from "../../constants";
 
 const styles = StyleSheet.create({
+  titleContainer: {
+    width: "100%",
+    padding: 10,
+    borderRadius:10,
+    marginBottom: 10,
+    alignItems: "center",
+    backgroundColor: COLORS.gray2,
+  },
+
+  container: {
+    width: "100%",
+    padding: 10,
+    marginBottom: 25,
+  },
+
+  titleText: {
+    fontSize: SIZES.large,
+    color: COLORS.secondary,
+  },
 });
 
 export default styles;
